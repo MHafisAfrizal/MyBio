8823af6df80d4106d571f289c4f695b45e92113d
MHafisAfrizal <hafisafrizal10@gmail.com>
2023-08-05 01:58:48 +0700

    first

---
+++ asseting/style.js
@@ -0,0 +1,29 @@
+const txtElement = ['CopyWriting', 'Student', 'Blogger', 'Web Developer', 'Network Enginer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function tik() {
+
+    if (count == txtElement.length) {
+        count = 0;
+    }
+
+    currentTxt = txtElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-tik').textContent = words;
+
+    if (words.length == currentTxt.length) {
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(tik, 500);
+
+
+})();
+
+//alert web
+alert("Selamat Datang di Profil saya...!, klik (OK) utuk melanjutkan");
